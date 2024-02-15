9d0ae75597c9c6daaf319b2ed880f7dad9affa6c
kaic nicoluci <kaic.nicolucci@gmail.com>
2024-02-15 14:44:28 -0300

    responsividade 2.0

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let carrinho = [];
 let addCart = document.querySelector('.row');
 
 inicializarLoja = () => {
-  let narguiles = document.querySelector('.narguiles');
+  let narguiles = document.querySelector('.produto');
 
   fetch('./produtos.json')
     .then(response => response.json())
@@ -25,24 +25,50 @@ inicializarLoja = () => {
           </div>
         `);
       });
-
-
-
-
-
-
-
-
-      $('.narguiles, .narguiles-grandes').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-      });
       $('.carrossel-informacoes-start').slick({
         autoplay: true
       })
-
+      $('.produto').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 625,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+              breakpoint: 321,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+          }
+        ]
+      });
+    
+    
     })
 
     .catch(error => console.error('Erro ao carregar o JSON:', error));
